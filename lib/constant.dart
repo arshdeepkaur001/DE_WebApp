f9e047a33a41c9450b81afea8097fd429d7594bd
Vanshika <103096079+SumanKumar891@users.noreply.github.com>
2024-01-09 21:01:10 +0530

    devices

--- lib/constant.dart
+++ lib/constant.dart
@@ -110,7 +110,8 @@ import 'dart:async';
 import 'package:http/http.dart' as http;
 
 List<Device> deviceData = [];
-final fmt = DateFormat("MMM d yyyy hh:mm:ss");
+final fmt1 = DateFormat("MMM d yyyy hh:mm:ss");
+final fmt = DateFormat('dd-MM-yyyy_HH-mm-ss');
 // COLORS
 const backgroundColor = Colors.white;
 
@@ -141,28 +142,40 @@ Future<String> isUserFound(String email, String psw) async {
 
 Future<String> getData(String email) async {
   final response = await http.get(
+    Uri.parse('https://2nd0kjysjg.execute-api.us-east-1.amazonaws.com/'),
+    // headers: {
+    //   'email': email,
+    // },
+  );
+  final response1 = await http.get(
     Uri.parse('https://fukaqwjsci.execute-api.us-east-1.amazonaws.com/data'),
     headers: {
       'email': email,
     },
   );
-  if (response.statusCode == 200) {
-    DateTime current = DateTime.now().toUtc();
-    String now = fmt.format(current);
-    // print(now);
-    int nowInMS = fmt.parse(now).millisecondsSinceEpoch;
+  // print(response.body);
+  // print(response1.body);
 
-    deviceData.clear();
-    for (var i in jsonDecode(response.body)) {
-      // print(i);
-      deviceData.add(Device.fromJson(i, nowInMS));
-    }
-    deviceData.sort((a, b) => a.deviceId.compareTo(b.deviceId));
-    deviceData.sort(compareDevices); // sort the list
-    return deviceData.isEmpty ? '400' : '200';
-  } else {
-    throw Exception('Failed to load api');
+  DateTime current = DateTime.now().toUtc();
+  String now = fmt.format(current);
+  // print(now);
+  int nowInMS = fmt.parse(now).millisecondsSinceEpoch;
+
+  deviceData.clear();
+  for (var i in jsonDecode(response.body)) {
+    // print(i);
+    deviceData.add(Device.fromJson(i, nowInMS));
   }
+  for (var i in jsonDecode(response1.body)) {
+    // print(i);
+    deviceData.add(Device.fromJson1(i, nowInMS));
+  }
+  deviceData.sort((a, b) => a.deviceId.compareTo(b.deviceId));
+  deviceData.sort(compareDevices); // sort the list
+  return deviceData.isEmpty ? '400' : '200';
+  // } else {
+  //   throw Exception('Failed to load api');
+  // }
 }
 
 class Device {
@@ -177,6 +190,29 @@ class Device {
   });
 
   factory Device.fromJson(Map<String, dynamic> dvc, nowInMS) {
+    if (dvc['lastReceivedTime'] == 'empty') {
+      return Device(
+        deviceId: dvc['DeviceId'],
+        registerStatus: true,
+        status: 'inactive',
+      );
+    } else {
+      String dt = dvc['lastReceivedTime'];
+      int previousDt = fmt.parse(dt).millisecondsSinceEpoch;
+      int distance = nowInMS - previousDt;
+
+      if (distance < 300000) {
+        return Device(
+            deviceId: dvc['DeviceId'], registerStatus: true, status: "active");
+      } else {
+        return Device(
+            deviceId: dvc['DeviceId'],
+            registerStatus: true,
+            status: 'inactive');
+      }
+    }
+  }
+  factory Device.fromJson1(Map<String, dynamic> dvc, nowInMS) {
     if (dvc['status'] == 'empty') {
       return Device(
         deviceId: dvc['device_id'],
@@ -185,10 +221,10 @@ class Device {
       );
     } else {
       String dt = dvc['status'].split(' ').sublist(1, 5).join(' ');
-      int previousDt = fmt.parse(dt).millisecondsSinceEpoch;
+      int previousDt = fmt1.parse(dt).millisecondsSinceEpoch;
       int distance = nowInMS - previousDt;
 
-      if (distance < 600000) {
+      if (distance < 300000) {
         return Device(
             deviceId: dvc['device_id'],
             registerStatus: dvc['register_status'],
