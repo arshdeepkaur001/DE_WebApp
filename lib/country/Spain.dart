4c80805d306e0e15ad30ad027aaab7eb983b206a
Pallavi0119 <104244682+Pallavi0119@users.noreply.github.com>
2024-03-18 21:54:35 +0530

    Update Spain.dart

--- lib/country/Spain.dart
+++ lib/country/Spain.dart
@@ -316,7 +316,7 @@ class _SpainScreenState extends State<SpainScreen> {
                                 Navigator.of(context).push(
                                   MaterialPageRoute(
                                     builder: (_) => Battery(
-                                      deviceId: deviceData[i].deviceId,
+                                        deviceId: filterData[i].deviceId,
                                     ),
                                   ),
                                 );
